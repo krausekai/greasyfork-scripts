12886e70d6f4eaa44a886743d09047d39edc0b2c
krausekai <25758458+krausekai@users.noreply.github.com>
2018-03-29 03:47:03 +1100

    Localization Update

--- Quizlet - Hotkeys to Search Flash Cards by Accuracy/Quizlet - Hotkeys to Search Flash Cards by Accuracy.js
+++ Quizlet - Hotkeys to Search Flash Cards by Accuracy/Quizlet - Hotkeys to Search Flash Cards by Accuracy.js
@@ -5,31 +5,32 @@
 // @author      Kai Krause <kaikrause95@gmail.com>
 // @match       http://quizlet.com/*flash-cards/*
 // @match       https://quizlet.com/*flash-cards/*
-// @version     1.4
+// @version     1.5
 // @grant       none
 // ==/UserScript==
 
 window.onload = function(){
 	var titles = document.getElementsByClassName("SetPageTermChunk-title");
+	var titlesCache = [];
 
 	for (var i = 0; i < titles.length; i++){
 		titles[i].innerHTML += "<a style='visibility:hidden' name='" +  titles[i].textContent + "'></a>";
+		titlesCache.push(titles[i].textContent);
 	}
-
-	window.addEventListener("keydown", titleSelect, true);
 
 	function titleSelect(charE) {
 		var el = document.activeElement;
 		if (el.tagName.toLowerCase() != 'textarea') {
 			if (charE.keyCode == "49") {
-				window.location.hash = "Often Missed";
-			}else if (charE.keyCode == "50") {
-				window.location.hash = "Sometimes Missed";
-			}else if (charE.keyCode == "51") {
-				window.location.hash = "Never Missed";
-			}else if (charE.keyCode == "52") {
-				window.location.hash = "No Answers Yet";
+				window.location.hash = titlesCache[0];
+			} else if (charE.keyCode == "50") {
+				window.location.hash = titlesCache[1];
+			} else if (charE.keyCode == "51") {
+				window.location.hash = titlesCache[2];
+			} else if (charE.keyCode == "52") {
+				window.location.hash = titlesCache[3];
 			}
 		}
 	}
+	window.addEventListener("keydown", titleSelect, true);
 }
