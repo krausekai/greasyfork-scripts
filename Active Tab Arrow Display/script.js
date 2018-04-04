de3209185b74ce96ead382381dd4505253539fe7
krausekai <25758458+krausekai@users.noreply.github.com>
2018-04-05 00:42:25 +1000

    dynamic update

--- Active Tab Arrow Display/script.js
+++ Active Tab Arrow Display/script.js
@@ -2,26 +2,43 @@
 // @name         Active Tab Arrow Display
 // @namespace    ActiveTabArrowDisplay
 // @description  Display an arrow on the title of the currently active tab
-// @version      1.0
+// @version      1.1
 // @author       Kai Krause <kaikrause95@gmail.com>
 // @include      *
 // @run-at       document-end
 // ==/UserScript==
 
-var title = document.title;
-var modifier = "➜ ";
+var modifier = "➜";
 
 function flashTitle() {
 	setTimeout(() => {
+		var title = document.title;
+		title = title.replace(modifier,"");
+
+		if (title[0] == modifier) {
+			title = title.substr(2);
+		}
 		if (document.hasFocus() || !document.hidden) {
-			document.title = modifier + title;
+			document.title = modifier + " " + title;
 		} else if (!document.hasFocus() && document.hidden) {
 			document.title = title;
 		}
 	}, 500);
 }
 
-flashTitle(title);
+flashTitle();
 
 window.addEventListener("focus", flashTitle, true);
 window.addEventListener("blur", flashTitle, true);
+
+var titleSel = document.querySelector('title');
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var MutationObserverConfig={
+    childList: true,
+    subtree: true,
+    characterData: true
+};
+var titleObserver = new MutationObserver(function(mutations){
+	flashTitle();
+});
+titleObserver.observe(titleSel, MutationObserverConfig);
