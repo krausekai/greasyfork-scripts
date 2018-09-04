2dd66b870643d5c04a8134db6e000822668392b0
krausekai <25758458+krausekai@users.noreply.github.com>
2018-09-04 18:23:14 +1000

    disable speed review timer

--- Memrise Timer Disabler/Memrise Timer Disabler.js
+++ Memrise Timer Disabler/Memrise Timer Disabler.js
@@ -1,23 +1,17 @@
 // ==UserScript==
 // @name           Memrise Timer Disabler
-// @description    Disables the Timer on watering & gardening levels in Memrise.com
+// @description    Disables the Timer on Memrise.com
 // @match          http://*.memrise.com/*
 // @match          https://*.memrise.com/*
-// @version        0.1.6
+// @version        0.2.1
 // @grant          none
 // @namespace      https://greasyfork.org/users/3656
 // ==/UserScript==
 
-var onLoad = function($) {
-	$("div.garden-timer div.txt").bind("DOMSubtreeModified", function() {
-		//MEMRISE.garden.timer.cancel();
-		MEMRISE.garden._events.pause[0]();
-	});
-};
-
-var injectWithJQ = function(f) {
-	var script = document.createElement('script');
-	script.textContent = '$(' + f.toString() + '($));';
-	document.body.appendChild(script);
-};
-injectWithJQ(onLoad);
+function callback() {
+	MEMRISE.garden.$speedtimer = null;
+  MEMRISE.garden._events.pause[0]();
+}
+var observer = new MutationObserver(callback);
+var config = { attributes: true, childList: true, subtree: true };
+observer.observe(document.body, config);
