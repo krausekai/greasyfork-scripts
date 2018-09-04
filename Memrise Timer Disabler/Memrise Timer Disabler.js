e73db2e565f19f617c4bcbe7f6c6e414be55288f
krausekai <25758458+krausekai@users.noreply.github.com>
2018-09-05 02:04:24 +1000

    disable speed timer more cleanly

--- Memrise Timer Disabler/Memrise Timer Disabler.js
+++ Memrise Timer Disabler/Memrise Timer Disabler.js
@@ -3,14 +3,16 @@
 // @description    Disables the Timer on Memrise.com
 // @match          http://*.memrise.com/*
 // @match          https://*.memrise.com/*
-// @version        0.2.1
+// @version        0.2.2
 // @grant          none
 // @namespace      https://greasyfork.org/users/3656
 // ==/UserScript==
 
 function callback() {
-	MEMRISE.garden.$speedtimer = null;
 	MEMRISE.garden._events.pause[0]();
+	MEMRISE.garden.session.timer.countdown = false;
+	MEMRISE.garden.$speedbg[0] = ""
+	//MEMRISE.garden.$speedtimer = {}
 }
 var observer = new MutationObserver(callback);
 var config = { attributes: true, childList: true, subtree: true };
