e1ad12d12680661116003acf4db1bc84eceffe74
krausekai <25758458+krausekai@users.noreply.github.com>
2019-04-06 18:08:49 +1100

    Improved site detection & Multilingual hide button detection

--- YouTube Subscriptions Page Hide Viewed Videos/YouTube Subscriptions Page Hide Viewed Videos.js
+++ YouTube Subscriptions Page Hide Viewed Videos/YouTube Subscriptions Page Hide Viewed Videos.js
@@ -2,10 +2,10 @@
 // @name         YouTube Subscriptions Page: Hide Viewed Videos
 // @namespace    hideViewedVideos_kk
 // @description  Once a video is clicked, it will be hidden from the subscription page
-// @version      0.6
+// @version      0.7
 // @author       Kai Krause <kaikrause95@gmail.com>
-// @match        http://www.youtube.com/feed/subscriptions*
-// @match        https://www.youtube.com/feed/subscriptions*
+// @match        http://*.youtube.com/*
+// @match        https://*.youtube.com/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
@@ -53,26 +53,31 @@ function autoHideClicked (e) {
 	if (target.href && (target.href.includes('/user/') || target.href.includes('/channel/'))) return;
 	// Disable video menu clicks from removing the video, and ignore the thumbnail 'play' animation
     if (target.tagName === "BUTTON" || target.tagName === "YT-ICON" && target.id !== "play") return;
-	
+
 	while (target) {
 		if (target.tagName === "YTD-GRID-VIDEO-RENDERER") {
-			
+
 			var hideMenuButton = target.getElementsByTagName('button')[0];
 			hideMenuButton.click();
-			
+
 			setTimeout(function() {
 				// Hide the video via the youtube menus, because 1) lazy, 2) easier to update in future
 				var hideMenu = document.getElementsByTagName('ytd-popup-container')[0];
 				var hideButton = hideMenu.getElementsByTagName('yt-formatted-string');
+				hideButton[hideButton.length-1].click();
+
+				/*
 				for (var i = 0; i < hideButton.length; ++i) {
 					if (hideButton[i].innerHTML === "Hide") {
 						hideButton[i].click();
 						break;
 					}
 				}
+				*/
+
 				autoHideHidden(e);
 			}, 4);
-			
+
 			break;
 		} else {
 			target = target.parentNode;
